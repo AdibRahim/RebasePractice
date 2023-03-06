007abda28a8d935b249431a75b16da422420f685
AdibRahim <adib.rahim07@gmail.com>
2023-03-07 00:31:07 +0800

    updated pom file

--- git.sh
+++ git.sh
@@ -1,1 +1,4 @@
-git add . && git commit -m "updated pom file" && git push && AdibRahim && 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
+#!/bin/sh
+git add . && git commit -m "updated pom file" && git push && echo "AdibRahim" && echo "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"
+
+# if
